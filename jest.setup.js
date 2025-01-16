92b0ac06687594f131daae08feaa9300a0031631
yogeshHumancloud <yogesh.vitekar@humancloud.co.in>
2025-01-16 17:32:18 +0530

    added test config

---
+++ jest.setup.js
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+
+{
+  "compilerOptions": {
+    "types": ["jest", "@testing-library/jest-dom"],
+    "baseUrl": ".",
+    "paths": {
+      "@/*": ["./*"]
+    }
+  }
+}
